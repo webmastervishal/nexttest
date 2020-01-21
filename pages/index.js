c1bc0f06141869588dc360668fd56464815eb621
Vishal Shetty <vishalshetty1990@gmail.com>
2020-01-21 11:30:31 +0530

    Added support for styled components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Card, Button } from 'antd';
 
 export default () => {
     return <>
         <Card>
-            <h1>Welcome to Neptune</h1>
+            <Title>Welcome to Neptune</Title>
             <Button type="danger">Click me!</Button>
         </Card>
     </>
 }
+
+const Title = styled.h1`
+    color: #FFB300;
+`;  
