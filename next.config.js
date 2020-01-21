7694533d6b3876e0db076d5bd05212c417288446
Vishal Shetty <vishalshetty1990@gmail.com>
2020-01-21 11:20:53 +0530

    Next JS setup with Antd

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+    /* eslint-disable */
+    const withLess = require('@zeit/next-less')
+    const lessToJS = require('less-vars-to-js')
+    const fs = require('fs')
+    const path = require('path')
+
+    // fix: prevents error when .less files are required by node
+    if (typeof require !== 'undefined') {
+        require.extensions['.less'] = file => { }
+    }
+    return withLess({
+        lessLoaderOptions: {
+            javascriptEnabled: true
+        }
+    })
+};
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, Button } from 'antd';
+
+export default () => {
+    return <>
+        <Card>
+            <h1>Welcome to Neptune</h1>
+            <Button type="danger">Click me!</Button>
+        </Card>
+    </>
+}
