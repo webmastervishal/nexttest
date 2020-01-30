14c9a6cf8c2a6aef9d3422e948910968fb2a7489
shahrukhanwar <shahrukhanwer92@gmail.com>
2020-01-30 13:26:23 +0530

    feat: added phone number edit modal

--- components/Profile/ContactDetails.js
+++ components/Profile/ContactDetails.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Input } from "antd";
+
 import {
   CardX,
   ListX,
@@ -7,15 +10,36 @@ import {
   IconX
 } from "./style";
 import EditModal from "./EditModal";
+import EditForm from "./EditForm";
 
 const ContactDetails = () => {
+  const [editPhoneModalVisible, seteditPhoneModalVisible] = useState(false);
+  const [phoneValue, setphoneValue] = useState(null);
   return (
     <CardX>
       <ListX header={<ListHeader>Contact Details</ListHeader>}>
         <ListItemX>
           <TypographyTextX>Phone Number</TypographyTextX>
           <span>
-            9999999999 <IconX type="edit" theme="filled" />
+            9999999999
+            <IconX
+              type="edit"
+              theme="filled"
+              onClick={() => seteditPhoneModalVisible(true)}
+            />
+            <EditModal
+              title="Phone Number"
+              visible={editPhoneModalVisible}
+              setvisible={seteditPhoneModalVisible}
+            >
+              <EditForm>
+                <Input
+                  placeholder="Enter Phone Number"
+                  value={phoneValue}
+                  onChange={e => setphoneValue(e.target.value)}
+                />
+              </EditForm>
+            </EditModal>
           </span>
         </ListItemX>
         <ListItemX>
