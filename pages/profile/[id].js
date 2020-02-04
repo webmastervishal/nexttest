900bd42190806bb9989331c6a9cf88ffc891de22
Vishal Shetty <vishalshetty1990@gmail.com>
2020-02-04 10:42:11 +0530

    Done some changes in webpack configuration so that existing antd style can be overridden

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,6 +1,5 @@
 import MasterLayout from "./../../components/Common/Layout";
 import Profile from "./../../components/Profile";
-import "../../node_modules/antd/dist/my-theme.less";
 
 export default () => (
   <MasterLayout>
