ff93a4b104417e70c8eec7f9010bd0c1c6c5ff36
Vishal Shetty <vishalshetty1990@gmail.com>
2020-02-04 16:03:29 +0530

    Added some style for rendering xaxis labels properly, Added some style to card

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,29 +1,65 @@
+import { PureComponent } from 'react';
 import { Card, Row, Col } from "antd";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 const data = [
   {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    name: '1st', Vishal: 3.5, Shahrukh: 4, amt: 2400,
   },
   {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    name: '2nd', Vishal: 4.5, Shahrukh: 4.7, amt: 2210,
   },
   {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    name: '3rd', Vishal: 5, Shahrukh: 4.9, amt: 2290,
   },
   {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    name: '4th', Vishal: 3, Shahrukh: 4.2, amt: 2000,
   },
   {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    name: '5th', Vishal: 4.9, Shahrukh: 4, amt: 2181,
   },
   {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    name: '6th', Vishal: 2.8, Shahrukh: 4, amt: 2500,
   },
   {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    name: '7th', Vishal: 4, Shahrukh: 2, amt: 2100,
   },
+  {
+    name: '8th', Vishal: 2.5, Shahrukh: 4, amt: 2100,
+  },
+  {
+    name: '9th', Vishal: 2.9, Shahrukh: 3.9, amt: 2100,
+  },
+  {
+    name: '10th', Vishal: 3.2, Shahrukh: 4.4, amt: 2100,
+  },
+  {
+    name: '11th', Vishal: 3.9, Shahrukh: 2.6, amt: 2100,
+  },
+  {
+    name: '12th', Vishal: 4.9, Shahrukh: 5, amt: 2100,
+  },
+  {
+    name: '13th', Vishal: 5, Shahrukh: 4.5, amt: 2100,
+  },
+  {
+    name: '14th', Vishal: 2, Shahrukh: 3.9, amt: 2100,
+  }
 ];
+
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const {
+      x, y, stroke, payload,
+    } = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+      </g>
+    );
+  }
+}
 
 const renderLineChart = (
   <ResponsiveContainer width={'100%'} height={300}>
@@ -36,12 +72,12 @@ const renderLineChart = (
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
+      <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
+      <YAxis width={10} />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <Line type="monotone" dataKey="Vishal" stroke="#8884d8" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="Shahrukh" stroke="#82ca9d" />
     </LineChart>
   </ResponsiveContainer>
 );
@@ -49,7 +85,7 @@ const renderLineChart = (
 const Dashboard = () => {
   return <Row>
     <Col span={24}>
-      <Card title="Instructor Ratings" >
+      <Card title="Instructor Ratings" headStyle={{ fontWeight: 300 }} style={{ boxShadow: '0px 6px 8px #e8e8e8' }}>
         {renderLineChart}</Card>
     </Col>
   </Row>;
