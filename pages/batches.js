fd3709dce122777bb313ca6986ba2663355cd69f
Vishal Shetty <vishalshetty1990@gmail.com>
2020-02-14 14:59:28 +0530

    Added batchcards to the batches page

--- pages/batches.js
+++ pages/batches.js
@@ -1,3 +1,49 @@
+import { Col } from 'antd';
+import { RowX } from './../components/Common/UIElements';
+import BatchCard from './../components/Common/BatchCard';
 import MasterLayout from './../components/Common/Layout';
 
-export default () => <MasterLayout><p>Batches</p></MasterLayout>
+export default () => <MasterLayout>
+
+  <RowX>
+    <Col span={24}>
+      <BatchCard
+        name="Eagle Batch"
+        status="danger"
+        startdate="1st Apr 2019"
+      />
+    </Col>
+  </RowX>
+
+
+  <RowX>
+    <Col span={24}>
+      <BatchCard
+        name="Flamingo Batch"
+        status="normal"
+        startdate="1st Oct 2019"
+      />
+    </Col>
+  </RowX>
+
+
+  <RowX>
+    <Col span={24}>
+      <BatchCard
+        name="Woodpecker Batch"
+        status="normal"
+        startdate="18th Nov 2019"
+      />
+    </Col>
+  </RowX>
+
+  <RowX>
+    <Col span={24}>
+      <BatchCard
+        name="Robin Batch"
+        status="danger"
+        startdate="1st Jan 2020"
+      />
+    </Col>
+  </RowX>
+</MasterLayout>
