7b40fbea5f159bb6398cc4baa1bc7dcff73774d9
Vishal Shetty <vishalshetty1990@gmail.com>
2020-02-18 10:23:22 +0530

    Moved html head section to document page

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./../apollo/client";
 import { GlobalStyle } from "./../components/Common/Layout/style";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <Head>
-        <link rel="shortcut icon" href="/images/logo.png" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </ApolloProvider>
---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/images/logo.png" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
